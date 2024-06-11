9f424696a9bf61385d27c0e0c731651e3104fd48
DuonggPh <104338962+DuonggPh@users.noreply.github.com>
2024-06-11 10:53:42 +0700

    update email services

--- emailservices/src/routes/email/index.js
+++ emailservices/src/routes/email/index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const EmailController = require("../../controllers/email.controller");
 const asyncHandler = require("../../helpers/asyncHandler");
 const router = express.Router();
-
 // Tìm người dùng bằng email
 router.post("/new_template", asyncHandler(EmailController.newTemplate));
 router.post("/verify-token", asyncHandler(EmailController.verifyToken));
